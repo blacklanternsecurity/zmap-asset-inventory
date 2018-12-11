7e31f060414bc505a7c4a1c062ef64f6aa594e66
TheTechromancer <joel.moore@blacklanternsecurity.com>
2018-12-11 10:34:17 -0500

    Added diff feature

--- zmap_asset_inventory.py
+++ zmap_asset_inventory.py
@@ -41,7 +41,7 @@ class Zmap:
 
         zmap_command = ['zmap', '--blacklist-file={}'.format(self.blacklist), \
             '--bandwidth={}'.format(self.bandwidth), \
-            '--probe-module=icmp_echoscan'] + self.targets
+            '--probe-module=icmp_echoscan'] + [str(t) for t in self.targets]
 
         print('\n[+] Running zmap:\n\t> {}\n'.format(' '.join(zmap_command)))
 
@@ -69,7 +69,7 @@ class Zmap:
     def hosts_sorted(self):
 
         hosts_sorted = list(self.hosts.values())
-        hosts_sorted.sort(key=lambda x: ipaddress.IPv4Address(x['IP Address']))
+        hosts_sorted.sort(key=lambda x: ipaddress.ip_address(x['IP Address']))
         return hosts_sorted
 
 
@@ -88,14 +88,16 @@ class Zmap:
         self.scanned_eternal_blue = True
 
 
-    def report(self):
+    def report(self, netmask=24):
 
         print('\n\n[+] RESULTS:')
         print('=' * 50)
         print('[+] Total Online Hosts: {:,}'.format(len(self.hosts)))
         print('[+] Summary of Subnets:')
-        for subnet in self._count_subnets():
-            print('\t{:<15}{:<10}'.format(subnet[0], ' ({:,})'.format(subnet[1])))
+        summarized_hosts = list(self.summarize_online_hosts(netmask=netmask).items())
+        summarized_hosts.sort(key=lambda x: x[1], reverse=True)
+        for subnet in summarized_hosts:
+            print('\t{:<19}{:<10}'.format(str(subnet[0]), ' ({:,})'.format(subnet[1])))
 
         if self.eternal_blue_count > 0:
             print('')
@@ -179,20 +181,96 @@ class Zmap:
                 self.blacklist = str(self.blacklist.resolve())
 
 
-    def _count_subnets(self):
+    def get_network_delta(self, sub_range_file, netmask=24):
+        '''
+        takes file containing newtork hosts/ranges
+        returns dictionary:
+        {
+            network: host_count,
+            ...
+        }
+        '''
+
+        stray_hosts = self.get_host_delta(sub_range_file)
+
+        sub_ranges = set()
+        with open(sub_range_file) as f:
+            lines = [line.strip() for line in f.readlines()]
+            for line in lines:
+                for network in str_to_network(line):
+                    sub_ranges.add(network)
+
+        stray_networks = list(self.summarize_online_hosts(hosts=stray_hosts, netmask=netmask).items())
+
+        return stray_networks
+
+
+
+    def get_host_delta(self, sub_host_file):
+
+        sub_ranges = set()
+        with open(sub_host_file) as f:
+            lines = [line.strip() for line in f.readlines()]
+            for line in lines:
+                try:
+                    for network in str_to_network(line):
+                        sub_ranges.add(network)
+                except:
+                    continue
+
+        master_ranges = [i[0] for i in self.summarize_online_hosts()]
+
+        hosts = [ipaddress.ip_address(i) for i in self.hosts]
+
+        stray_hosts = []
+        for h in hosts:
+            if not any([h in s for s in sub_ranges]):
+                stray_hosts.append(h)
+
+        stray_hosts.sort()
+        return stray_hosts
+
+
+    def summarize_online_hosts(self, hosts=None, netmask=24):
+
+        if hosts is None:
+            hosts = self.hosts
 
         subnets = dict()
 
         for ip in self.hosts:
-            subnet = '.'.join(ip.split('.')[:2]) + '.X.X'
+
+            subnet = ipaddress.ip_network(ip + '/{}'.format(netmask), strict=False)
+
             try:
                 subnets[subnet] += 1
             except KeyError:
                 subnets[subnet] = 1
 
-        subnets = list(subnets.items())
-        subnets.sort(key=lambda x: x[1], reverse=True)
         return subnets
+
+
+
+    @staticmethod
+    def _deduplicate_net_ranges(net_ranges):
+        '''
+        currently unused, but potentially useful
+        can't bring myself to delete it
+        '''
+
+        net_ranges = [ipaddress.ip_network(net) for net in net_ranges]
+        net_ranges.sort(key=lambda x: x.netmask, reverse=True)
+        deduplicated_ranges = []
+
+        for i in range(len(net_ranges)):
+
+            net_range = net_ranges[i]
+            # if network doesn't overlap with any larger ones
+            if not any([net_range.overlaps(n) for n in net_ranges[i+1:]]):
+                deduplicated_ranges.append(net_range)
+
+        return deduplicated_ranges
+
 
 
     def __iter__(self):
@@ -321,40 +399,17 @@ class Host(dict):
 
     def __str__(self):
 
-        return '{}{}'.format(self['IP Address'], (' ({})'.format(self['Hostname']) if self['Hostname'] else ''))
+        return '{:<16}{}'.format(self['IP Address'], self['Hostname'])
 
 
 
-if __name__ == '__main__':
+def main(options):
 
     if os.geteuid() != 0:
         sys.stderr.write('[!] Must be root\n')
-        sys.exit(1)
-
-    default_bandwidth = '500K'
-    default_work_dir = Path.home() / '.asset_inventory'
-    default_csv_file = default_work_dir / 'asset_inventory.csv'
-
-    parser = argparse.ArgumentParser("Scan private IP ranges, output to CSV")
-    parser.add_argument('-t', '--targets', nargs='+', default=['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16'], help='target network(s) to scan', metavar='STR')
-    parser.add_argument('-B', '--bandwidth', default=default_bandwidth,     help='max egress bandwidth (default {})'.format(default_bandwidth), metavar='STR')
-    parser.add_argument('--blacklist',      default=None,                   help='a file containing hosts to exclude from scanning', metavar='FILE')
-    parser.add_argument('-w', '--csv-file',                                 help='output CSV file', metavar='CSV_FILE')
-    parser.add_argument('-f', '--start-fresh',        action='store_true',  help='don\'t load results from previous scans')
-    parser.add_argument('-p', '--ports', nargs='+', type=int,               help='port-scan online hosts')
-    parser.add_argument('-e', '--check-eternal-blue', action='store_true',  help='scan for EternalBlue')
-    parser.add_argument('--work-dir',       type=Path,                      help='custom working directory')
+        sys.exit(2)
 
     try:
-
-        options = parser.parse_args()
-        options.targets = list(set(options.targets))
-        options.targets.sort()
-
-        if options.work_dir is None:
-            # unique identifier based on scan targets
-            scan_uid = '_'.join([t.replace('/', '-') for t in options.targets])
-            options.work_dir = default_work_dir / scan_uid
 
         # resolve symlinks
         options.work_dir = options.work_dir.resolve()
@@ -367,7 +422,9 @@ if __name__ == '__main__':
         if options.start_fresh:
             backup_work_dir = Path(str(options.work_dir) + '_{date:%Y-%m-%d_%H:%M:%S}.bak'.format( date=datetime.now() ))
             try:
+                old_dir = str(options.work_dir)
                 options.work_dir.rename(backup_work_dir)
+                print('[+] Backed up {} to {}'.format(old_dir, str(backup_work_dir)))
             except FileNotFoundError:
                 pass
 
@@ -410,18 +467,43 @@ if __name__ == '__main__':
                 z.scan_online_hosts(port)
 
         # print summary
-        z.report()
+        z.report(netmask=options.netmask)
+
+        # calculate deltas if requested:
+        if options.diff:
+            stray_hosts = []
+            stray_networks = []
+
+            stray_networks = z.get_network_delta(options.diff, netmask=options.netmask)
+            print('')
+            print('[+] {:,} active network(s) not found in {}'.format(len(stray_networks), str(options.diff)))
+            print('=' * 50)
+            with open('./network_diff_{date:%Y-%m-%d_%H:%M:%S}.csv'.format( date=datetime.now()), 'w', newline='') as f:
+                csv_file = csv.DictWriter(f, fieldnames=['Network'])
+                csv_file.writeheader()
+                for network in stray_networks:
+                    csv_file.writerow({'Network': str(network)})
+                    print('\t{}'.format(str(network)))
+
+            stray_hosts = z.get_host_delta(options.diff)
+            print('')
+            print('[+] {:,} alive host(s) not found in {}'.format(len(stray_hosts), str(options.diff)))
+            print('=' * 50)
+            with open('./host_diff_{date:%Y-%m-%d_%H:%M:%S}.csv'.format( date=datetime.now()), 'w', newline='') as f:
+                csv_file = csv.DictWriter(f, fieldnames=['IP Address', 'Hostname'])
+                csv_file.writeheader()
+                for host in stray_hosts:
+                    host = z.hosts[str(host)]
+                    csv_file.writerow({'IP Address': host['IP Address'], 'Hostname': host['Hostname']})
+                    print('\t{}'.format(str(host)))
+
+            print('\n')
+            # if more than 5 percent of hosts are strays, or you have more than one stray network
+            if len(stray_hosts)/len(z.hosts) > .05 or len(stray_networks) > 1:
+                print(' "Your asset management is bad and you should feel bad"')
+                print('')
 
         print('[+] CSV file written to {}'.format(options.csv_file))
-
-
-    except (argparse.ArgumentError, ValueError) as e:
-        sys.stderr.write('\n[!] {}\n'.format(str(e)))
-        sys.exit(2)
-
-    except KeyboardInterrupt:
-        sys.stderr.write('\n[!] Interrupted\n')
-        sys.exit(1)
 
     finally:
         try:
@@ -432,3 +514,84 @@ if __name__ == '__main__':
                 pickle.dump(z, f)
         except:
             pass
+
+
+def str_to_network(s):
+    '''
+    takes either CIDR or range notation as string
+    generates ip_network objects
+    '''
+
+    try:
+        if '-' in s:
+            start, end = s.split('-')
+            for i in ipaddress.summarize_address_range(ip_address(start), ip_address(end)):
+                yield i
+        else:
+            yield ipaddress.ip_network(s, strict=False)
+
+    except ValueError:
+        pass
+
+
+def parse_target_args(targets):
+    '''
+    takes 
+    '''
+
+    networks = set()
+
+    for network_list in targets:
+        for network in network_list:
+            networks.add(network)
+
+    networks = list(networks)
+    networks.sort()
+    return networks
+
+
+
+if __name__ == '__main__':
+
+    default_bandwidth = '750K'
+    default_work_dir = Path.home() / '.asset_inventory'
+    default_cidr_mask = 24
+
+    parser = argparse.ArgumentParser("Scan private IP ranges, output to CSV")
+    parser.add_argument('-t', '--targets', type=str_to_network, nargs='+', default=[['10.0.0.0/8'], ['172.16.0.0/12'], ['192.168.0.0/16']], help='target network(s) to scan', metavar='STR')
+    parser.add_argument('-B', '--bandwidth', default=default_bandwidth,         help='max egress bandwidth (default {})'.format(default_bandwidth), metavar='STR')
+    parser.add_argument('--blacklist',                                          help='a file containing hosts to exclude from scanning', metavar='FILE')
+    parser.add_argument('-w', '--csv-file',                                     help='output CSV file', metavar='CSV_FILE')
+    parser.add_argument('-f', '--start-fresh',          action='store_true',    help='don\'t load results from previous scans')
+    parser.add_argument('-p', '--ports', nargs='+', type=int,                   help='port-scan online hosts')
+    parser.add_argument('-e', '--check-eternal-blue',   action='store_true',    help='scan for EternalBlue')
+    parser.add_argument('--work-dir',               type=Path,                  help='custom working directory', metavar='DIR')
+    parser.add_argument('-d', '--diff',             type=Path,                  help='show differences between scan results and IPs/networks from file', metavar='FILE')
+    parser.add_argument('-n', '--netmask', type=int, default=default_cidr_mask, help='summarize networks with this CIDR mask (default: {})'.format(default_cidr_mask))
+
+    try:
+
+        options = parser.parse_args()
+        options.targets = parse_target_args(options.targets)
+        if not options.targets:
+            sys.stderr.write('\n[!] No valid targets\n')
+            sys.exit(1)
+
+        assert 0 <= options.netmask <=32, "Invalid netmask"
+
+        if options.work_dir is None:
+            # unique identifier based on scan targets
+            scan_uid = '_'.join([str(t).replace('/', '-') for t in options.targets])
+            options.work_dir = default_work_dir / scan_uid
+
+
+        main(options)
+
+
+    except (argparse.ArgumentError, ValueError, AssertionError) as e:
+        sys.stderr.write('\n[!] {}\n'.format(str(e)))
+        sys.exit(2)
+
+    except KeyboardInterrupt:
+        sys.stderr.write('\n[!] Interrupted\n')
+        sys.exit(1)
